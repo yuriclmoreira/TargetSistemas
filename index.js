5d8b639671d516c20be22fd55c7ec6192f5fd85e
Yuri Moreira <yuriclmoreira@terra.com.br>
2023-04-04 19:22:45 -0300

    Resolucao exercicio 4 v1

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-/*
-2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
-
-IMPORTANTE:
-Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;*/
+/* --------------------------------2-------------------------------- */
 
 function Fibonacci() {
   var numero = parseInt(document.getElementById("numero").value);
@@ -37,15 +33,7 @@ function Fibonacci() {
       "<h3>O número selecionado não esta na sequencia de fibonacci.</h3>";
   }
 }
-/*
-3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
-• O menor valor de faturamento ocorrido em um dia do mês;
-• O maior valor de faturamento ocorrido em um dia do mês;
-• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
-
-IMPORTANTE:
-a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
-b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;*/
+/* --------------------------------3-------------------------------- */
 async function getValores(valores) {
   try {
     const response = await fetch("dados.json");
@@ -93,4 +81,31 @@ async function Faturamento() {
     "O número de valores que ultrapasou  a media foi :" +
     contador +
     "</h3>";
+}
+/* --------------------------------4-------------------------------- */
+
+function Percentual() {
+  var respostaPercentual = document.getElementById("respostaPercentual");
+  const dados = [
+    { estado: "SP", valor: 67836.43 },
+    { estado: "RJ", valor: 36678.66 },
+    { estado: "MG", valor: 29229.88 },
+    { estado: "ES", valor: 27165.48 },
+    { estado: "Outros", valor: 19849.53 },
+  ];
+
+  const total = dados.reduce((acc, curr) => acc + curr.valor, 0);
+  const percentuais = dados.map((item) =>
+    ((item.valor / total) * 100).toFixed(2)
+  );
+
+  let html = "<ul>";
+
+  for (let i = 0; i < dados.length; i++) {
+    html += `<li>${dados[i].estado}: R$ ${dados[i].valor}: Percentual: R$ ${percentuais[i]}</li>`;
+  }
+
+  html += "</ul>";
+
+  respostaPercentual.innerHTML = html;
 }
